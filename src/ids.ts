6791c97e1a7a3e9a8841da2dc69df07d1de24039
Jason Godesky <jefgodesky@gmail.com>
2025-11-25 17:30:35 -0500

    Roll table IDs from compendium

---
+++ src/ids.ts
@@ -0,0 +1,49 @@
+export default {
+  british: {
+    ships: {
+      merchant: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.aZK5OtHswteWMHGE',
+      naval: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.EahUnhxUbipKWkUB'
+    },
+    given: {
+      fem: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.rPQW2jBoqSwIrxIQ',
+      masc: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.4GZWNpzu1WESf9ni'
+    },
+    surnames: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.Sp0FfupHVKM2O6DZ'
+  },
+  dutch: {
+    ships: {
+      merchant: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.DqNXgJXlhRghfnXV',
+      naval: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.mMjZBr0BmbeKmu3d'
+    },
+    given: {
+      fem: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.fnbsMLjOBf8qdyN4',
+      masc: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.bPw3LQSI57PLWJX9'
+    },
+    surnames: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.O7SzIfgGvASHAqCc'
+  },
+  french: {
+    ships: {
+      merchant: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.YUsZW4uJi0vaWUBx',
+      naval: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.oMpo7YaOXzZ3yzH8'
+    },
+    given: {
+      fem: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.aFmWKPGZB5IoTMsK',
+      masc: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.Ei8dDYiw4SB27YB0'
+    },
+    surnames: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.0PoHJ1aYEB9Yf728'
+  },
+  spanish: {
+    ships: {
+      merchant: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.lUWNYPUQ00zOSIAP',
+      naval: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.8aMg2fkRkX8Gnjmz',
+      religious: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.Fn4TeNwEZoZV2gXd'
+    },
+    given: {
+      fem: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.vqZvraZ0rXAjIBzK',
+      masc: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.3auqdwjOBeCF6uzA'
+    },
+    surnames: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.3auqdwjOBeCF6uzA'
+  },
+  gender: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.gnMiX5FE6YAZhNEV',
+  nationality: 'Compendium.revolutionary-piratenames.piratenames-tables.RollTable.WBDDsvsA9tQBDKiF'
+}
